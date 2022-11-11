e51d8b41b3aae12b23ea867e76d095807a201bc0
MexL <madiajijah7@gmail.com>
2022-11-11 17:48:49 +0800

    add more code explain

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,19 +19,21 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    // check if email and password are true
     if (email === "admin@gmail.com" && password === "admin123") {
         window.location.href = "homepage.html";
-        return;
+    } else {
+        // show alert bellow the form with red color
+        const alert = document.createElement("div");
+        alert.style.color = "red";
+        alert.style.border = "1px solid red";
+        alert.style.padding = "10px";
+        alert.style.margin = "10px 0";
+        alert.innerHTML = "Email or password is wrong";
+        form.appendChild(alert);
+        // remove alert after 3 seconds
+        setTimeout(() => {
+            alert.remove();
+        }, 3000);
     }
-    // show alert bellow the form with red color
-    const alert = document.createElement("div");
-    alert.style.color = "red";
-    alert.style.border = "1px solid red";
-    alert.style.padding = "10px";
-    alert.style.margin = "10px 0";
-    alert.innerHTML = "Email or password is wrong";
-    form.appendChild(alert);
-    setTimeout(() => {
-        alert.remove();
-    }, 3000);
 });
